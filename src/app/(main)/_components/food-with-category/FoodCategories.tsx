26be0ae0221abcbfa21e0fc459996690a89644ea
25LP8050 25LP8050 <25LP8050@admins-iMac.local>
2025-07-02 12:50:06 +0800

    arg

--- src/app/(main)/_components/food-with-category/FoodCategories.tsx
+++ src/app/(main)/_components/food-with-category/FoodCategories.tsx
@@ -1,42 +1,45 @@
 "use client";
-
+import React, { useEffect, useState } from "react";
+import cors from "cors";
 import { FoodsWithCategories } from "./FoodsWithCategories";
 
-const categories = [
-  {
-    categoryName: "categoryName1",
-    _id: 1,
-  },
-  {
-    categoryName: "categoryName2",
-    _id: 2,
-  },
-  {
-    categoryName: "categoryName3",
-    _id: 3,
-  },
+type Categories = {
 
-  {
-    categoryName: "categoryName4",
-    _id: 4,
-  },
-];
+_id:Number
+categoryName:String
+createdAt:Number
+updatedAt:Number
+__v:Number
+
+}
 
 export const FoodCategories = () => {
-  if (!categories.length)
-    return <p className="text-white">No categories found</p>;
+  const[categories, setCategories] = useState<Categories[]>([]);
+  useEffect(() => {
+    const getCategories = async () => { 
+      const response = await fetch("http://localhost:3001/food-category")
+      const data = await response.json();
+      console.log("Categorydataa",data);
 
+      setCategories(data)
+      
+  };
+  getCategories();
+},[]);
+  if (!categories.foodCategories?.length)
+    return <p className="text-white">No categories found</p>;
+console.log(categories?.foodCategories)
   return (
     <div>
       <div className="flex flex-col my-8 gap-9">
         <div className="text-3xl font-semibold text-white">Categories</div>
         <div className="flex gap-2 flex-nowrap">
-          {categories?.map((category) => (
+          {categories?.foodCategories.map((category) => (
             <div
               key={category._id}
               className="flex items-center px-5 py-1 rounded-full bg-background"
             >
-              <div>{category?.categoryName}</div>
+              <div>{category.categoryName}</div>
             </div>
           ))}
         </div>
