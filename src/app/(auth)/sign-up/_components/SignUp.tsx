fe807f6a23bddd6acc280572c25ef371f781d6b3
25LP8050 25LP8050 <25LP8050@admins-iMac.local>
2025-07-24 13:37:05 +0800

    sigin in

--- src/app/(auth)/sign-up/_components/SignUp.tsx
+++ src/app/(auth)/sign-up/_components/SignUp.tsx
@@ -4,13 +4,66 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { SignUpEmailBox } from "./SignUpEmailBox";
 import { SignUpPasswordBox } from "./SignUpPasswordBox";
+import { toast } from "sonner";
+import { useFormik } from "formik";
+import { DataTable } from "@/components/admin/orders/data-table";
 
 export const Signup = () => {
   const { push } = useRouter();
+  
+  const [email, setEmail] = useState<string>("");
+
+  const [password, setPassword] = useState<string>("");
+
+  const signUp = async (values: { email?: string; password?: string }) => {
+    console.log(values);
+    try {
+      const response = await fetch("http://localhost:3001/user", {
+        method: "POST",
+        body: JSON.stringify({
+          email: values.email,
+          password: values.password,
+          address: "address",
+          phoneNumber: "phoneNumber",
+        }),
+        headers: {
+          "content-type": "application/json;  charset=UTF-8",
+        },
+      });
+      const data = await response.json();
+      console.log("response", data);
+
+      if (!response.ok) {
+        throw new Error("Failed to create category");
+      }
+      toast.success("successfully created user");
+
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      await signUp(values);
+    },
+  });
 
   const StepComponents = [
-    <SignUpEmailBox key={0} />,
-    <SignUpPasswordBox key={1} />,
+    <SignUpEmailBox
+      key={0}
+      values={formik.values}
+      errors={formik.errors}
+      touched={formik.touched}
+      handleChange={formik.handleChange}
+      onSubmit={formik.handleSubmit}
+    />,
+    // <SignUpPasswordBox key={1} values={formik.values} />,
   ];
 
   return StepComponents;
--- src/app/(auth)/sign-up/_components/SignUpEmailBox.tsx
+++ src/app/(auth)/sign-up/_components/SignUpEmailBox.tsx
@@ -5,11 +5,13 @@ import { DynamicCardHeader } from "@/components/card";
 import { Card, CardContent } from "@/components/ui/card";
 import { FormInput } from "../../../../components/dynamic-inputs";
 import { SignUpFooter } from "./SignUpFooter";
+import { useState } from "react";
+import { toast } from "sonner";
 
 type EmailBoxProps = {
-  values: { email: string };
-  errors: { email?: string };
-  touched: { email?: boolean };
+  values: { email: string; password: string };
+  errors: { email?: string; password?: string };
+  touched: { email?: string; password?: boolean };
   handleChange: (_event: React.ChangeEvent<HTMLInputElement>) => void;
   handleBlur: (_event: React.FocusEvent<HTMLInputElement>) => void;
   handleNext: () => void;
@@ -34,7 +36,15 @@ export const SignUpEmailBox = ({
     inputError: formError,
     inputErrorMessage: errors.email,
   };
-
+  const passwordInputProps = {
+    name: "password",
+    placeholder: "Password",
+    value: values.password,
+    onChange: handleChange,
+    onBlur: handleBlur,
+    inputError: formError,
+    inputErrorMessage: errors.password,
+  };
   return (
     <Card className="w-[416px] border-none shadow-none gap-6 flex flex-col">
       <DynamicCardHeader
@@ -42,10 +52,11 @@ export const SignUpEmailBox = ({
         description="Sign up to explore your favorite dishes."
       />
       <CardContent className="p-0">
-        <form>
+        <form >
           <div className="flex flex-col gap-6">
             <div className="grid items-center w-full gap-6">
               <FormInput {...emailInputProps} />
+              <FormInput {...passwordInputProps} />
             </div>
             <FooterButtons buttonText="Let`s Go" />
           </div>
